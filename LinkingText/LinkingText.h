a147ef5017f7e4b4f4ea6d7d57210de7d5ba906c
Whitney Foster <whitney.m.foster@gmail.com>
2016-09-29 11:11:01 -0700

    rename and code clean

---
+++ LinkingText/LinkingText.h
@@ -0,0 +1,19 @@
+//
+//  LinkingText.h
+//  LinkingText
+//
+//  Created by Whitney Foster on 9/28/16.
+//  Copyright © 2016 whitney.io. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LinkingText.
+FOUNDATION_EXPORT double LinkingTextVersionNumber;
+
+//! Project version string for LinkingText.
+FOUNDATION_EXPORT const unsigned char LinkingTextVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LinkingText/PublicHeader.h>
+
+
